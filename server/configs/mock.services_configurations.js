f50f0df1321696913368740704b37e7cc2b9d885
gotrecillo <sergio.panadero.perez@gmail.com>
2016-05-21 11:37:31 +0200

    mock services configurations

--- server/configs/mock.services_configurations.js
+++ server/configs/mock.services_configurations.js
@@ -1,1 +1,35 @@
-mock.services_configurations.js
+import { ServiceConfiguration } from 'meteor/service-configuration';
+
+export default function () {
+  ServiceConfiguration.configurations.update(
+    { service: 'twitter' },
+    {
+      service: 'twitter',
+      consumerKey: 'twitterKey',
+      loginStyle: 'popup',
+      secret: 'twitterSecret'
+    },
+    { upsert: true }
+  );
+
+  ServiceConfiguration.configurations.update(
+    { service: 'facebook' },
+    {
+      service: 'facebook',
+      appId: 'facebookKey',
+      secret: 'facebookSecret'
+    },
+    { upsert: true}
+  );
+
+  ServiceConfiguration.configurations.update(
+    { service: 'google' },
+    {
+      service: 'google',
+      clientId: 'googleKey',
+      secret: 'googleSecret'
+    },
+    { upsert: true}
+  );
+
+}
